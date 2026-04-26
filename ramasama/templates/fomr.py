9a4d5d1ab47d208ef13f1e07f744acd61e1308f5
Jareotsen <pierrotjokiassama@gmail.com>
2026-04-26 12:19:16 +0100

    deplace Base.html de includes vers template modif de liste_formulaire et form.py

--- ramasama/templates/fomr.py
+++ ramasama/templates/fomr.py
@@ -5,7 +5,9 @@ from .models import Vente
 class VenteForm(forms.ModelForm):
     class Meta:
         model = Vente
+        #on liste nos champs de la table vente
         fields = ['produit', 'vendeur', 'quantite', 'montant', 'date']
+        #Ajout des classes ccs de boostrap pour le style rama je suis nul en design oh
         widgets = {
             'produit': forms.Select(attrs={'class': 'form-select'}),
             'vendeur': forms.Select(attrs={'class': 'form-select'}),
@@ -13,3 +15,8 @@ class VenteForm(forms.ModelForm):
             'montant': forms.NumberInput(attrs={'class': 'form-control'}),
             'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
+def clean_quantite(self):
+        quantite = self.cleaned_data.get('quantite')
+        if quantite <= 0:
+            raise forms.ValidationError("La quantité doit être supérieure à zéro.")
+        return quantite
